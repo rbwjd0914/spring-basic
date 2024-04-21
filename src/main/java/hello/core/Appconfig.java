8bb05e8f994a53fa4b7629f663e0f7c8f7832ce8
rbwjd0914 <ajttnd@naver.com>
2024-04-21 17:47:39 +0900

    @Configuration은 바이트코드를 조작하는 CGLIB 기술을 통해 싱글톤을 보장한다.

--- src/main/java/hello/core/Appconfig.java
+++ src/main/java/hello/core/Appconfig.java
@@ -12,16 +12,24 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Appconfig {
+
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
     @Bean
     public MemberService memberService(){
+        System.out.println("call Appconfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
-    public static MemoryMemberRepository memberRepository() {
+    // static 메서드는 싱글톤을 지원하지 않는다. 새로운 객체 생성
+    public MemoryMemberRepository memberRepository() {
+        System.out.println("call Appconfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService(){
+        System.out.println("call Appconfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
